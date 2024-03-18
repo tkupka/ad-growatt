7ecd59626ab4148ed61c0e7b6464b00f5323e957
Tomas Kupka <tomas.kupka@mgm-tp.com>
2024-03-18 12:32:01 +0100

    Satet logger

---
+++ appdaemon/apps/stateLogger.py
@@ -0,0 +1,23 @@
+name = "stateLogger"
+
+import time
+import datetime
+
+class StateLogger:
+    
+    hass = ''
+    
+
+    def __init__(self,  hass):
+        self.hass = hass
+
+
+    def info(self, message):
+        self.hass.set_state("sensor.template_adgw_api_state", state = self.format_message(message))
+        
+    def error(self, message):
+        self.hass.set_state("sensor.template_adgw_api_state", state = self.format_message("Error: %s"%message))
+        
+    def format_message(self, message):
+        timestamp = datetime.datetime.now().strftime('%H:%M')
+        return "%s [%s]" %(message, timestamp)
